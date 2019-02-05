8fa995298f1dd315e4e5fecaccc0282fd98900fd
cjh <1418503647@qq.com>
2019-02-05 11:54:20 +0800

    mac

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <my-page title="首页">
+        这是首页内容
+    </my-page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                page: {
+                    menu: [
+                        {
+                            type: 'icon',
+                            icon: 'help',
+                            to: '/help'
+                        }
+                    ]
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
